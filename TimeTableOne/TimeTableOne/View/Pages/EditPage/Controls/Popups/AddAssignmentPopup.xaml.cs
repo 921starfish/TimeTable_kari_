000a041e0fe38708f14d66b6ebcdefa4c05954e0
LimeStreem <LimeStreem@gmail.com>
2014-12-24 00:04:58 +0900

    Test of ContextMenu

--- TimeTableOne/TimeTableOne/View/Pages/EditPage/Controls/Popups/AddAssignmentPopup.xaml.cs
+++ TimeTableOne/TimeTableOne/View/Pages/EditPage/Controls/Popups/AddAssignmentPopup.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,6 +23,24 @@ namespace TimeTableOne.View.Pages.EditPage.Controls.Popups
         public AddAssignmentPopup()
         {
             this.InitializeComponent();
+        }
+
+        public AddAssignmentPopupViewModel ViewModel
+        {
+            get { return DataContext as AddAssignmentPopupViewModel; }
+        }
+
+        private async void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            Button b = sender as Button;
+            Point transformPoint = b.TransformToVisual(Window.Current.Content).TransformPoint(new Point(0, 0));
+
+
+            PopupMenu menu = new PopupMenu();
+            menu.Commands.Add(new UICommand("テスト1"));
+            menu.Commands.Add(new UICommand("テスト2"));
+            await menu.ShowAsync(transformPoint);
+
         }
     }
 }
---
+++ TimeTableOne/TimeTableOne/View/Pages/EditPage/Controls/Popups/AddAssignmentPopupViewModel.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimeTableOne.View.Pages.TablePage.Controls;
+
+namespace TimeTableOne.View.Pages.EditPage.Controls.Popups
+{
+    public class AddAssignmentPopupViewModel:BasicViewModel
+    {
+        protected string _assignmentName;
+        protected string _assignmentDetail;
+        protected string _yearEdit;
+        protected string _monthEdit;
+        protected string _dayEdit;
+        protected DateTime _dueDate;
+
+        public string AssignmentName
+        {
+            get { return _assignmentName; }
+            set
+            {
+                if (value == _assignmentName) return;
+                _assignmentName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string AssignmentDetail
+        {
+            get { return _assignmentDetail; }
+            set
+            {
+                if (value == _assignmentDetail) return;
+                _assignmentDetail = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string YearEdit
+        {
+            get { return _yearEdit; }
+            set
+            {
+                if (value == _yearEdit) return;
+                _yearEdit = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string MonthEdit
+        {
+            get { return _monthEdit; }
+            set
+            {
+                if (value == _monthEdit) return;
+                _monthEdit = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string DayEdit
+        {
+            get { return _dayEdit; }
+            set
+            {
+                if (value == _dayEdit) return;
+                _dayEdit = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public DateTime DueDate
+        {
+            get { return _dueDate; }
+            set
+            {
+                if (value.Equals(_dueDate)) return;
+                _dueDate = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    class AddAssignmentPopupViewModelInDesign : AddAssignmentPopupViewModel
+    {
+        public AddAssignmentPopupViewModelInDesign()
+        {
+            _assignmentName = "前期レポート課題";
+            _yearEdit = "2015";
+            _monthEdit = "12";
+            _dayEdit = "25";
+            _assignmentDetail = "計算数学のレポート課題";
+        }
+    }
+}
