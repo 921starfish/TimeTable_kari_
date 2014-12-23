bd6c1e404b930db572c6f410a949f6987f991b1a
LimeStreem <LimeStreem@gmail.com>
2014-12-23 21:12:48 +0900

    アニメーション適用

--- TimeTableOne/TimeTableOne/View/Pages/TablePage/TablePageViewModel.cs
+++ TimeTableOne/TimeTableOne/View/Pages/TablePage/TablePageViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
+using Windows.UI.StartScreen;
 using TimeTableOne.Data;
 using TimeTableOne.Utils;
 using TimeTableOne.View.Pages.TablePage.Controls;
@@ -9,10 +10,15 @@ namespace TimeTableOne.View.Pages.TablePage
     public class TablePageViewModel : BasicViewModel
     {
         private TimeTableGridViewModel _timeTableDataContext;
+        private string _pageTitleForEdit;
+
         public TablePageViewModel()
         {
             TimeTableDataContext=new TimeTableGridViewModel();
             Width = TableLayoutManager.getElementWidth(ApplicationData.Instance.Configuration.TableTypeSetting);
+            var config = ApplicationData.Instance.Configuration;
+            PageTitleForEdit=PageTitle = config.PageTitle;
+            
         }
 
         public TimeTableGridViewModel TimeTableDataContext
@@ -27,6 +33,23 @@ namespace TimeTableOne.View.Pages.TablePage
         }
 
         public int Width { get; set; }
+
+        public string PageTitle { get; set; }
+
+        public string PageTitleForEdit
+        {
+            get { return _pageTitleForEdit; }
+            set
+            {
+                if (value == _pageTitleForEdit||string.IsNullOrEmpty(value)) return;
+                _pageTitleForEdit = value;
+                PageTitle = value;
+                ApplicationData.Instance.Configuration.PageTitle = value;
+                ApplicationData.SaveData(ApplicationData.Instance);
+                OnPropertyChanged("PageTitle");
+                OnPropertyChanged();
+            }
+        }
     }
 
     public class TablePageViewModelInDesign : TablePageViewModel
