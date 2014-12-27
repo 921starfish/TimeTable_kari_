c571f8439613c0f7a4c6a4d400465a6353c48c38
LimeStreem <LimeStreem@gmail.com>
2014-12-27 18:39:57 +0900

    implement NoClassControlViewModel

---
+++ TimeTableOne/TimeTableOne/View/Pages/EditPage/Controls/Units/NoClassRoomListUnitViewModel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+using TimeTableOne.View.Pages.TablePage.Controls;
+
+namespace TimeTableOne.View.Pages.EditPage.Controls.Units
+{
+    public class NoClassRoomListUnitViewModel:BasicViewModel
+    {
+        private string _displayDate;
+        private string _supportText;
+        private Brush _noClassStateBrush;
+        private string _noClassStateText;
+
+        public NoClassRoomListUnitViewModel()
+        {
+            
+        }
+
+        public Brush NoClassStateBrush
+        {
+            get { return _noClassStateBrush; }
+            set
+            {
+                if (Equals(value, _noClassStateBrush)) return;
+                _noClassStateBrush = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string NoClassStateText
+        {
+            get { return _noClassStateText; }
+            set
+            {
+                if (value == _noClassStateText) return;
+                _noClassStateText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string DisplayDate
+        {
+            get { return _displayDate; }
+            set
+            {
+                if (value == _displayDate) return;
+                _displayDate = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string SupportText
+        {
+            get { return _supportText; }
+            set
+            {
+                if (value == _supportText) return;
+                _supportText = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    class NoClassRoomListUnitViewModelInDesign : NoClassRoomListUnitViewModel
+    {
+        public NoClassRoomListUnitViewModelInDesign()
+        {
+            DisplayDate = DateTime.Now.ToString("M月dd日");
+            SupportText = "次";
+            NoClassStateBrush=new SolidColorBrush(Colors.Red);
+            NoClassStateText = "休講";
+        }
+    }
+}
