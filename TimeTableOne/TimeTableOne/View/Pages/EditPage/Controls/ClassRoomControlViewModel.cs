45ec1e81fd185830772255943b532c7fca4d353a
LimeStreem <LimeStreem@gmail.com>
2014-12-27 16:42:49 +0900

    implement viewmodel of change classroom

---
+++ TimeTableOne/TimeTableOne/View/Pages/EditPage/Controls/ClassRoomControlViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimeTableOne.View.Pages.TablePage.Controls;
+
+namespace TimeTableOne.View.Pages.EditPage.Controls
+{
+    public class ClassRoomControlViewModel:BasicViewModel
+    {
+        public ClassRoomControlViewModel()
+        {
+            
+        }
+
+        public string RecordFirstDate { get; set; }
+    }
+
+    public class ClassRoomControlViewModelInDesign : ClassRoomControlViewModel
+    {
+        public ClassRoomControlViewModelInDesign()
+        {
+            RecordFirstDate = DateTime.Now.ToString("M月dd日からの記録");
+        }
+    }
+}
