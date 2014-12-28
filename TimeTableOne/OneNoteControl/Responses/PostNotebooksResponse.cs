8104cf52f4acff2ae8744062b734804647d4af88
921starfish <yuya.921.hoshino@gmail.com>
2014-12-28 19:28:12 +0900

    Making PostNotebooksResponse.cs

---
+++ TimeTableOne/OneNoteControl/Responses/PostNotebooksResponse.cs
@@ -0,0 +1,38 @@
+﻿using Windows.UI.Xaml;
+using Newtonsoft.Json;
+using OneNoteControl.Responses;
+
+namespace OneNoteControl.Responses
+{
+    public class PostNotebooksResponse
+    {
+        [JsonProperty(PropertyName = "@odata.context")]
+        public string context;
+
+        public bool isDafult;
+
+        public string userRole;
+
+        public bool isShared;
+
+        public string sectionUrl;
+
+        public string sectionsGroupsUrl;
+
+        public LinksUnit links;
+
+        public string id;
+
+        public string name;
+
+        public string self;
+
+        public string createdBy;
+
+        public string lastModifiedBy;
+
+        public string createdTime;
+
+        public string lastModifiedTime;
+    }
+}
