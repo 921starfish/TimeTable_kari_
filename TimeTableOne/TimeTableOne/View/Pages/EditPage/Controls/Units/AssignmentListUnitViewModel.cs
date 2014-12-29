86978580f19d772946c292a153683621784a8070
921starfish <yuya.921.hoshino@gmail.com>
2014-12-29 15:00:36 +0900

    buildエラーを修正

--- TimeTableOne/TimeTableOne/View/Pages/EditPage/Controls/Units/AssignmentListUnitViewModel.cs
+++ TimeTableOne/TimeTableOne/View/Pages/EditPage/Controls/Units/AssignmentListUnitViewModel.cs
@@ -159,7 +159,7 @@ namespace TimeTableOne.View.Pages.EditPage.Controls.Units
             this.RemainingDateInformation = "残り1日";
             this.AssignmentStatusColor = new SolidColorBrush(Colors.DarkOrange);
             this.AssignmentStatus = "未完了";
-            this.AssignmentDetail = "This is detail.\n This is detail.\nThis is detail.\nThis is detail.";
+            this.AssignmentDetail = "This is detail.\nThis is detail.\nThis is detail.\nThis is detail.";
         }
     }
 }
