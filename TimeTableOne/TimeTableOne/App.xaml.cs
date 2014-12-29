01e59e2e9f7efdbe6939d3374a86e5c7bfd3dd6b
921starfish <yuya.921.hoshino@gmail.com>
2014-12-30 01:42:53 +0900

    プライバシーポリシーの作成

--- TimeTableOne/TimeTableOne/App.xaml.cs
+++ TimeTableOne/TimeTableOne/App.xaml.cs
@@ -96,17 +96,6 @@ namespace TimeTableOne
 
 
 			SettingsPane.GetForCurrentView().CommandsRequested += onCommandsRequested;
-            SettingsPane.GetForCurrentView().CommandsRequested += App_CommandsRequested;
-        }
-
-        void App_CommandsRequested(SettingsPane sender, SettingsPaneCommandsRequestedEventArgs args)
-        {
-            args.Request.ApplicationCommands.Add(new SettingsCommand("Privacy", "プライバシーポリシー", onSettingsCommand));
-        }
-
-        async void onSettingsCommand(IUICommand command)
-        {
-            await Launcher.LaunchUriAsync(new Uri("http://921starfish.github.io/TimeTable_kari_"));
         }
 
         /// <summary>
