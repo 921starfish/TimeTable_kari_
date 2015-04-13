2540fd92df69b9728428657e7caa79a943290d66
MrYD <azelf.trickroom@gmail.com>
2015-04-13 10:47:24 +0900

    TextManagerの追加

---
+++ TimeTableOne/TimeTableOne/Common/TextManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeTableOne.Common
+{
+    public enum Language
+    {
+        Japanese,
+        English,
+        Chinese
+    };
+
+    static class TextManager
+    {
+        static TextManager()
+        {
+            SetLanguage(Language.Japanese);
+        }
+
+        public static void SetLanguage(Language language)
+        {
+            switch (language)
+            {
+                case Language.Japanese:
+                    break;
+                case Language.English:
+                    break;
+                case Language.Chinese:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException("language");
+            }
+        }
+
+        public static void SetJapanese()
+        {
+        }
+
+        public static void SetEnglish()
+        {
+
+        }
+
+        public static void SetChinese()
+        {
+        }
+    }
+}
